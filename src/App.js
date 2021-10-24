94ece417fcbf5580969d35b0b6d3614b7729a8d9
BinhPham <cbinh951>
2021-10-24 22:38:49 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState, useCallback } from 'react';
+import List from './List';
+import './styles.css';
+
+function App() {
+  const [books, setBooks] = useState([]);
+  const [courses, setCourses] = useState([]);
+
+  const onAddBook = useCallback((item) => {
+    const updatedItems = [...books, { item, id: `book-${books.length + 1}` }];
+    setBooks(updatedItems);
+  }, []);
+
+  const onAddCourse = useCallback((item) => {
+    const updatedItems = [
+      ...courses,
+      { item, id: `course-${courses.length + 1}` },
+    ];
+    setCourses(updatedItems);
+  }, []);
+
+  // const onAddBook = (item) => {
+  //   const updatedItems = [...books, { item, id: `book-${books.length + 1}` }];
+  //   setBooks(updatedItems);
+  // };
+
+  // const onAddCourse = (item) => {
+  //   const updatedItems = [
+  //     ...courses,
+  //     { item, id: `course-${courses.length + 1}` },
+  //   ];
+  //   setCourses(updatedItems);
+  // };
+
+  return (
+    <main className="App">
+      <section>
+        <h3> Books </h3>
+        <List onAddItem={onAddBook} items={books} listKey="books" />
+      </section>
+      <section>
+        <h3> Courses </h3>
+        <List onAddItem={onAddCourse} items={courses} listKey="courses" />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,22 @@
+import { memo } from "react";
+
+import AddItem from "./AddItem";
+
+const List = (props) => {
+  const { items } = props;
+
+  console.log("List rendered: ", props.listKey);
+
+  return (
+    <>
+      <AddItem onAddItem={props.onAddItem} placeholder="Add book" />
+      <ul>
+        {items.map((item) => {
+          return <li key={item.id}>{item.item}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default memo(List);
